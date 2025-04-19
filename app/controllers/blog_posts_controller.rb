006ad0ab9db3873e2762b54905993dd6563e8d76
TakuyaHarayama <44060633+harayama2@users.noreply.github.com>
2025-04-19 21:02:12 +0900

    Add edit and update actions

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -9,6 +9,10 @@ class BlogPostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
   def new
     @blog_post = BlogPost.new
   end
@@ -19,6 +23,15 @@ class BlogPostsController < ApplicationController
       redirect_to @blog_post
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
